b0e941cc0dc85074bf4bc0a684b5c5685ce6b03c
Matt Johnson <joh02864@umn.edu>
2018-12-05 00:57:00 -0600

    stuff

--- train_nonormal.py
+++ train_nonormal.py
@@ -1,4 +1,5 @@
 import pose_learn as pl
+import tensorflow as tf
 
 
 
@@ -8,8 +9,9 @@ pl.normalize = False
 
 X_train, Y_train, X_test, Y_test = pl.prepare_sets(json_dir, win_size)
 model = pl.build_model(win_size)
-model.fit(X_train, Y_train, validation_data=(X_test, Y_test),\
-          batch_size=128, epochs=100, verbose=2)
+with tf.device('/gpu:2'): 
+    model.fit(X_train, Y_train, validation_data=(X_test, Y_test),\
+              batch_size=128, epochs=100, verbose=2)
 
 model.save('nonormal_model.h5')
 
