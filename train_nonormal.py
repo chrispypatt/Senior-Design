25e954a449c09d342e865a512facf4f70d72c9a2
Matt Johnson <joh02864@umn.edu>
2018-12-05 18:35:14 -0600

    fixed tabs

--- train_nonormal.py
+++ train_nonormal.py
@@ -11,9 +11,9 @@ X_train, Y_train, X_test, Y_test = pl.prepare_sets(json_dir, win_size)
 model = pl.build_model(win_size)
 with tf.device('/gpu:2'): 
     for i in range(0, 10): 
-	print('Training for 10 epochs starting from epoch {}'.format(i*10))
+        print('Training for 10 epochs starting from epoch {}'.format(i*10))
         model.fit(X_train, Y_train, validation_data=(X_test, Y_test),\
                   batch_size=128, epochs=10, verbose=2)
-	print('{} epochs completed'.format((i + 1) * 10))
-	model.save('nonormal_model.h5')
+        print('{} epochs completed'.format((i + 1) * 10))
+        model.save('nonormal_model.h5')
 
