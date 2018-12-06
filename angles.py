dc00a1334a8fdf7c0348f6408a5d596678ae8bd0
Matt Johnson <joh02864@umn.edu>
2018-12-05 18:32:06 -0600

    updated angles.py

--- angles.py
+++ angles.py
@@ -25,86 +25,91 @@ def write_to_frame(video, angles, freq, behaviors):
 
     # read in video
     cap = cv2.VideoCapture(video.path + "/" + video.video_name + ".avi") 
-    # capture output video; 0 instead of video name -> webcam
-    # used to write new video
+
     # define codec
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
 
-    #(grabbed, frame) = cap.read()
-    #fshape = frame.shape
-    #fheight = fshape[0]
-    #fwidth = fshape[1]
+    # Get height and width of frame 
     fwidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     fheight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print('frame width: {}, frame height: {}'.format(fwidth, fheight))
+    print('Frame size: {}x{}'.format(fwidth, fheight))
+
+    # Set the position of the text elements and text parameters 
+    behavior_pos = (10, 80) 
+    angle_pos = (10, fheight - 120)
+    freq_pos = (10, fheight - 40)
+    font = cv2.FONT_HERSHEY_DUPLEX      # set font
+    font_size = 4
 
     # define codec, create VideoWriter object; output stored in 'newvideo.avi' file
     out = cv2.VideoWriter(video.path +  "/" + video.video_name +'new.avi',\
                           fourcc, fps, (fwidth,fheight)) 
-    # VideoWriter(filename, fourcc, fps, frameSize)
 
-    # capture output video; 0 instead of video name -> webcam
-    # Why do we do this again?
-    cap = cv2.VideoCapture(video.path + "/" + video.video_name + ".avi") 
-
-    font = cv2.FONT_HERSHEY_SIMPLEX	# set font
-    font_size = 4
-
-    # iterate through whole video
     count = 0
-    while(cap.isOpened()): # while video opened?
-        ret, frame = cap.read()		# read in frame
+    while(True):
+        # Read in frame. If no frame, break out of loop
+        ret, frame = cap.read()	
         if not ret: 
             break
         
-        # display text and stuff on frame
-        # if keypoints exist
-
-        # Write angle information 
-        if count < len(angles):
-            if angles[count] > 90.0:
-                cv2.putText(frame, "angle: %.2f" % angles[count],\
-                            (10, 80), font, font_size, (0,0,255), 2)
-            else:
-                cv2.putText(frame, "angle: %.2f" % angles[count],\
-                            (10, 80), font, font_size, (255,255,0), 2)
-
-        # Write frequency information 
-        cv2.putText(frame, "freq: %.2f" % freq[count],\
-                    (10, fheight - 40), font, font_size, (255,255,0), 2)
-
         # Write behavior classification
         cv2.putText(frame, "behavior: {}".format(behaviors[count]),\
-                    (10, fheight - 120), font, font_size, (255, 255, 0), 2)
+                    behavior_pos, font, font_size, (255, 255, 0), 2)
 
-        frame_pose = video.get_window() 
-        if (frame_pose != False):
-            cv2.line(frame,\
-                     (int(frame_pose[RElbow][0]), int(frame_pose[RElbow][1])),\
-                     (int(frame_pose[Neck][0]), int(frame_pose[Neck][1])),\
-                     (0, 0, 255), 
-                     2)
-            cv2.line(frame,\
-                     (int(frame_pose[Neck][0]), int(frame_pose[Neck][1])),\
-                     (int(frame_pose[MHip][0]), int(frame_pose[MHip][1])),\
-                     (0,0,255),
-                     2)
+        # Write angle, freq, and draw angle if behavior is hand waving
+        if behaviors[count] == 'handwaving':
+            # Write angle information 
+            if count < len(angles):
+                if angles[count] > 90.0:
+                    cv2.putText(frame, "angle: %.2f" % angles[count],\
+                                angle_pos, font, font_size, (0,0,255), 2)
+                else:
+                    cv2.putText(frame, "angle: %.2f" % angles[count],\
+                                angle_pos, font, font_size, (255,255,0), 2)
 
+            # Write frequency information 
+            cv2.putText(frame, "freq: %.2f" % freq[count],\
+                        freq_pos, font, font_size, (255,255,0), 2)
+
+
+
+            pose = video.get_window() 
+            if (pose != False and has_keypoint(frame, RElbow)\
+                              and has_keypoint(frame, Neck)
+                              and has_keypoint(frame, MHip)):
+                # Draw neck to elbow line 
+                cv2.line(frame,\
+                         keypoint_int(pose, RElbow), keypoint_int(pose, Neck),\
+                         (0, 0, 255), 2)
+                
+                # Draw neck to hip line 
+                cv2.line(frame,\
+                         keypoint_int(pose, Neck), keypoint_int(pose, MHip),\
+                         (0,0,255), 2)
+
+        # Write new frame to output and increment count 
+        out.write(frame)
         count += 1
-        # if angle >= angle_threshc
-        # cv2.rectangle(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-        # if freq >= freq_thresh
-        # cv2.rectangle(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-        #cv2.imshow('Frame',frame)
 
-        out.write(frame)		# write new frame into the new file newvideo.avi
-
+        # Stop transfer when 'q' is pressed  
         if (cv2.waitKey(50) & 0xFF)== ord('q'):	# press 'q' to exit
             break
+
+        # Loop end
 
     cap.release() # release video capture object
     out.release()
     cv2.destroyAllWindows() # close frames
+
+
+# Checks that the pose has a particular keypoint
+def has_keypoint(pose, keypoint):
+    return pose[keypoint][2] > 0
+
+
+# Returns a tuple (int(x), int(y)) for keypoint with given index
+def keypoint_int(pose, keypoint): 
+    return (int(pose[keypoint][0]), int(pose[keypoint][1]))
 
 
 #create_2Dvector(p1,p2)
