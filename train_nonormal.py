8d5cd820c19f9392e92fc99b5ef330b89a71c8ab
Matt Johnson <joh02864@umn.edu>
2018-12-05 18:33:56 -0600

    nonormal

--- train_nonormal.py
+++ train_nonormal.py
@@ -10,8 +10,10 @@ pl.normalize = False
 X_train, Y_train, X_test, Y_test = pl.prepare_sets(json_dir, win_size)
 model = pl.build_model(win_size)
 with tf.device('/gpu:2'): 
-    model.fit(X_train, Y_train, validation_data=(X_test, Y_test),\
-              batch_size=128, epochs=100, verbose=2)
-
-model.save('nonormal_model.h5')
+    for i in range(0, 10): 
+	print('Training for 10 epochs starting from epoch {}'.format(i*10))
+        model.fit(X_train, Y_train, validation_data=(X_test, Y_test),\
+                  batch_size=128, epochs=10, verbose=2)
+	print('{} epochs completed'.format((i + 1) * 10))
+	model.save('nonormal_model.h5')
 
